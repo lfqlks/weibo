0b77fbb083e56894546501397563275b92b61d5f
Hugo <hugo@hugodemacbook-pro.local>
2016-10-08 15:09:04 +0800

    创建一个猫类

---
+++ text/text/Cat.h
@@ -0,0 +1,13 @@
+//
+//  Cat.h
+//  text
+//
+//  Created by Hugo on 16/10/8.
+//  Copyright © 2016年 Hugo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cat : NSObject
+
+@end
---
+++ text/text/Cat.m
@@ -0,0 +1,13 @@
+//
+//  Cat.m
+//  text
+//
+//  Created by Hugo on 16/10/8.
+//  Copyright © 2016年 Hugo. All rights reserved.
+//
+
+#import "Cat.h"
+
+@implementation Cat
+
+@end
