08476fac433aede619f115d23b8bd4d388dc8448
Hugo <hugo@hugodemacbook-pro.local>
2016-10-08 15:04:06 +0800

    初始化项目

---
+++ text/text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text
+//
+//  Created by Hugo on 16/10/8.
+//  Copyright © 2016年 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
